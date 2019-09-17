dc0523d0758bad2ee00f44c5fa669b2d2da8776b
Brdnlau <Brdnlau@gmail.com>
2019-09-17 01:11:06 -0700

    test1

--- Litter.java
+++ Litter.java
@@ -12,38 +12,52 @@ public class Litter extends Actor
      * Act - do whatever the Litter wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
-    public int [][] randomLitter = new int [15][2];
-    public boolean setup = true;
+    private static int [][] randomLitter = new int [15][2];
+    private static boolean setup = true;
+    private String [] trash = {"blah.png","blah2.jpg"};
+    private String [] recycle = {"blah.png","blah2.jpg"};
+    private String [] compost = {"blah.png","blah2.jpg"};
+    private int classification, type;
+    private static int iteration = 0;
     
     public void gameSetup() {
-        if(Greenfoot.isKeyDown("f")){
-            for (int i=0;i<randomLitter.length;i++) {
-                int classification = (int)(Math.random()*3 + 1);
-                if (classification == 1) {
-                    int picture = (int)(Math.random()*9 + 1);
-                    randomLitter[i][0] = 1;
-                    randomLitter[i][1] = picture;
-                }
-                else if (classification == 2) {
-                    int picture = (int)(Math.random()*9 + 1);
-                    randomLitter[i][0] = 2;
-                    randomLitter[i][1] = picture;
-                }
-                else if (classification == 3){
-                    int picture = (int)(Math.random()*9 + 1);
-                    randomLitter[i][0] = 3;
-                    randomLitter[i][1] = picture;
-                }
+        for (int i=0;i<randomLitter.length;i++) {
+            int imagesNumber = 9;
+            int classification = (int)(Math.random()*3 + 1);
+            if (classification == 1) {
+                int picture = (int)(Math.random()*imagesNumber + 1);
+                randomLitter[i][0] = 1;
+                randomLitter[i][1] = picture;
             }
-        
+            else if (classification == 2) {
+                int picture = (int)(Math.random()*imagesNumber + 1);
+                randomLitter[i][0] = 2;
+                randomLitter[i][1] = picture;
+            }
+            else if (classification == 3){
+                int picture = (int)(Math.random()*imagesNumber + 1);
+                randomLitter[i][0] = 3;
+                randomLitter[i][1] = picture;
+            }
         }
         setup = false;
     }
-     public int[][] getArray() {
-         return randomLitter;
-        }
-        
-
+    
+    public int[][] getLitter() {
+        return randomLitter;
+    }
+    
+    public Litter() {
+        classification = randomLitter[iteration][0];
+        type = randomLitter[iteration][1];
+        iteration++;
+        if (classification == 1) 
+            setImage(trash[type]);
+        else if (classification == 2)
+            setImage(recycle[type]);
+        else if (classification == 3)
+            setImage(compost[type]);
+    }
     
     public void act() 
     {
